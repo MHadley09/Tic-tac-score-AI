9b316ec335745450bfefce5c89c165b04526556b
MHadley09 <michael.hadley09@gmail.com>
2023-08-05 22:34:43 -0700

    Update config.py
    updating config for training mode

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #### SELF PLAY
 EPISODES = 30
-MCTS_SIMS = 1000
+MCTS_SIMS = 100
 MEMORY_SIZE = 6000
 TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
 CPUCT = 1
 EPSILON = 0.2
 ALPHA = 0.8
-DIRICHLET = False # set to false when testing, true when training
+DIRICHLET = True # set to false when testing, true when training
 
 #### PRINTING GAME
 PRINT_GAME = False # set to false when training, true when testing
